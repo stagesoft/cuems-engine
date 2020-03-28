c5d2562bfa6ee5809b9628658ea4552259f76a08
Ion Reguera <ion@stagelab.net>
2020-03-28 23:41:34 +0100

    Read / write xml from /to python dict

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ settings = {
     "node_id" : 0,
     "internal_osc_dest_host" : "localhost",
     "internal_osc_in_port" : 7700,
-    "internal_osc_out_port" : 7700,
+    "internal_osc_out_port" : 7701,
     "video_osc_port" : 7710,
     "videoplayer_path" : "/home/ion/src/xjadeo/src/xjadeo/xjadeo",
     "number_of_displays" : 2
--- xml_utils.py
+++ xml_utils.py
@@ -1,17 +1,43 @@
 import xml.etree.ElementTree as ET
 import datetime  as DT
+import config
 #tree = ET.parse('settings.xml')
 #root = tree.getroot()
 
+def write(settings_dict):
+    a = ET.Element('a')
+    a.tag = "Settings"
+    a.attrib['date'] = str(DT.datetime.now())
+    
 
 
-a = ET.Element('a')
-a.tag = "Settings"
-a.attrib['date'] = str(DT.datetime.now())
-a.text = "blu"
-b = ET.SubElement(a, 'b')
-b.tag = "inner"
-b.text = "contenido"
-ET.dump(a)
-tree=ET.ElementTree(a)
-tree.write('test.xml')
+    for node, node_id_dict in settings_dict.items():
+
+        for node_id, values_dict in node_id_dict.items():
+
+            b= ET.SubElement(a, node, id = node_id)
+
+            for key, value in values_dict.items():
+                c= ET.SubElement(b, key)
+                c.text = str(value)
+
+    ET.dump(a)
+    tree=ET.ElementTree(a)
+    tree.write('write_test.xml')
+
+def read():
+    settings = {}
+    tree = ET.parse('test.xml')
+    root = tree.getroot()
+    for child in root:
+        if not child.tag in settings:
+            settings.update({child.tag : {} })
+        
+        settings[child.tag][child.attrib['id']] = {}
+        for child_ in child:
+            settings[child.tag][child.attrib['id']].update({child_.tag : child_.text})
+
+    print(settings)
+    return settings
+
+write(read())
