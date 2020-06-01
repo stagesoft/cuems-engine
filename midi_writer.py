5d55e327b9348797c1f8f63510a8023375fe3edd
Ion Reguera <ion@stagelab.net>
2020-06-01 12:19:47 +0200

    midi tests

--- midi_writer.py
+++ midi_writer.py
@@ -1,58 +1,13 @@
 from midiutil.MidiFile import MIDIFile
 from numpy import interp
-from mtc import *
+from CTimecode import CTimecode
+
+from DmxCue import *
 
 import random
 
 import pprint
 
-class Cue():
-    def __init__(self):
-        self._cue = dict()
-
-
-
-
-
-class DmxUniverse(dict):
-
-    def __init__(self, *arg,**kw):
-      super().__init__(*arg,**kw)
-    
-
-
-    def channel(self, channel):
-        return super().__getitem__(channel)
-
-    def set_channel(self, channel, value):
-        if value > 255:
-            value = 255
-        super().__setitem__(channel, value)
-
-    def setall(self, value):
-        for channel in range(512):
-            super().__setitem__(channel, value)
-        return self      #TODO: valorate return self to be able to do things like 'universe_full = DmxUniverse().setall(255)'
-
-class DmxScene(dict):
-    def __init__(self, *arg,**kw):
-      super().__init__(*arg,**kw)
-
-    def universe(self, num=0):
-        return super().__getitem__(num)
-      
-    def set_universe(self, universe, num=0):
-        super().__setitem__(num, universe)
-
-       
-
-        #merge two universes, priority on the newcoming
-    def merge_universe(self, universe, num=0):
-        super().__getitem__(num).update(universe)
-
-
-class DmxCue(Cue):
-        pass
 
 class DmxSceneList():
 
@@ -118,7 +73,8 @@ class DmxSceneList():
 
 
 
-scene = DmxScene({0:DmxUniverse({0:230, 1:230})})
+#scene = DmxScene({0:{0:0, 4:4, 1:230}, 3:DmxUniverse().setall(30)})
+scene = DmxScene({0:{0:0, 4:4, 1:230}, 3:DmxUniverse().set_channel(2,30).set_channel(0, 10)})
 
 
 scene2 = DmxScene({1:DmxUniverse({0:20, 1:20})})
@@ -133,25 +89,61 @@ universe[3]=255
 
 scene.set_universe(universe, 1)
 
-universe_full = DmxUniverse().setall(255)
+#universe_full = DmxUniverse().setall(255)
 
 #scene.set_universe(universe_full, 2)
-scene.merge_universe(scene[0], 1)
+#scene.merge_universe(scene[0], 1)
 
 
-scene_list = DmxSceneList()
-scene_list.add_event(CTimecode('00:00:01:00'), scene, 2, 3)
+#scene_list = DmxSceneList()
+#scene_list.add_event(CTimecode('00:00:01:00'), scene, 2, 3)
 
-scene_list.add_event(CTimecode(frames=51), scene2, 12, 13)
-scene_list.print_events()
+#scene_list.add_event(CTimecode(frames=51), scene2, 12, 13)
+#scene_list.print_events()
 
 a = CTimecode('00:00:01:00')
 b = CTimecode(frames=26)
 c = CTimecode('00:00:01:00', framerate=30)
-print(a.frame_number)
-print(c.frame_number)
-print(a.milliseconds)
-print(c.milliseconds)
+
 assert a == a
 assert a == b
 assert a != c
+
+cue = Cue(60)
+print(cue)
+cue2 = Cue('00:01:00:00')
+print(cue2)
+print(cue2['time'])
+
+print(cue['time'])
+assert cue['time'] == cue2['time']
+
+dc = DmxCue(CTimecode('00:00:06:00'), scene, in_time=2, out_time=3)
+print(dc)
+
+scene[0].set_channel(0, 255)
+print(universe)
+scene[0].update({0:99, 1:99, 2:99, 3:99, 4:99})
+
+
+assert dc.time == dc['time']
+
+from Settings import Settings
+
+s = Settings('settings.xsd', 'dmx.xml', dc)
+s.data2xml(dc)
+s.write()
+
+sr = Settings('dmx.xsd', 'dmx.xml')
+sr.read()
+print(sr)
+
+print("------------")
+
+#print(sr['dmx_scene']['DmxUniverse'][0]['DmxChannel'][0]['$'])
+DmxUniverse()
+
+
+
+rc = DmxCue(sr['time']['CTimecode'], DmxScene({0: {0: 0, 4: 4, 1: 230}, 3: {2: 30, 0: 10}, 1: {0: 10, 1: 15, 2: 15, 3: 255}}), in_time=sr['in_time'], out_time=sr['out_time'])
+print(rc)
