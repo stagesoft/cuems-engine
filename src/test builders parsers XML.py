2a045ccc87102f68367ca4a839023f83744227e7
Ion Reguera <ion@stagelab.net>
2020-10-19 17:00:03 +0200

    Update Parser/Builder and Writer/Reader to the new project schema

--- src/test builders parsers XML.py
+++ src/test builders parsers XML.py
@@ -10,39 +10,40 @@ from cuems.DictParser import CuemsParser
 from cuems.XmlBuilder import XmlBuilder
 from cuems.XmlReaderWriter import XmlReader, XmlWriter
 
+import xml.etree.ElementTree as ET
 
 
-c = Cue(33, {'type': 'mtc', 'loop': 'False', 'media': "file.ext"})
-c.outputs = {'id': 5, 'bla':'ble'}
-c2 = Cue(None, {'type': 'floating', 'loop': 'False'})
-c2.outputs = {'physiscal': 1, 'virtual': 3}
-c3 = Cue(5, {'type': 'virtual', 'loop': 'False'})
-c3.outputs = {'id': 3}
-ac = AudioCue(45, {'type': 'virtual','loop': 'True'} )
-ac.outputs = {'stereo': 1}
-d_c = DmxCue(time=23, scene={0:{0:10, 1:50}, 1:{20:23, 21:255}, 2:{5:10, 6:23, 7:125, 8:200}})
-d_c.outputs = {'universe0': 3}
-g = Cue(33, {'type': 'mtc', 'loop': 'False'})
 
-custom_cue_list = CueList([c, c2])
+c = Cue(33, {'loop': False})
+c2 = Cue(None, { 'loop': False})
+c3 = Cue(5, {'loop': False})
+ac = AudioCue(45, {'loop': True, 'media': 'file.ext', 'master_vol': 66} )
+
+#ac.outputs = {'stereo': 1}
+#d_c = DmxCue(time=23, scene={0:{0:10, 1:50}, 1:{20:23, 21:255}, 2:{5:10, 6:23, 7:125, 8:200}}, init_dict={'loop' : True})
+#d_c.outputs = {'universe0': 3}
+g = Cue(33, {'loop': False})
+
+#custom_cue_list = CueList([c, c2])
+custom_cue_list = CueList( c )
+custom_cue_list.append(c2)
 custom_cue_list.append(ac)
+#custom_cue_list.append(d_c)
 
-custom_cue_list + [d_c, c3, g]
-float_cue_list = CueList([d_c, c3])
 
-float_cuelist = CueList([ac, c3 ])
-script = CuemsScript(timecode_cuelist=custom_cue_list, floating_cuelist=float_cue_list)
+script = CuemsScript(cuelist=custom_cue_list)
 script.name = "Test Script"
 print('OBJECT:')
 print(script)
 
 xml_data = XmlBuilder(script).build()
 
-writer = XmlWriter(schema = '/home/ion/src/cuems/python/osc-control/src/cuems/cues.xsd', xmlfile = '/home/ion/src/cuems/python/osc-control/src/cuems/cues.xml')
+
+writer = XmlWriter(schema = '/home/ion/src/cuems/python/cuems-engine/src/cuems/cues.xsd', xmlfile = '/home/ion/src/cuems/python/cuems-engine/src/cuems/cues.xml')
 
 writer.write(xml_data)
 
-reader = XmlReader(schema = '/home/ion/src/cuems/python/osc-control/src/cuems/cues.xsd', xmlfile = '/home/ion/src/cuems/python/osc-control/src/cuems/cues.xml')
+reader = XmlReader(schema = '/home/ion/src/cuems/python/cuems-engine/src/cuems/cues.xsd', xmlfile = '/home/ion/src/cuems/python/cuems-engine/src/cuems/cues.xml')
 xml_dict = reader.read()
 print("-------++++++---------")
 print('DICT from XML:')
@@ -62,17 +63,11 @@ else:
 
 
 print('xxxxxxxxxxxxxxxxxxxx')
-for o in store.timecode_cuelist:
+for o in store.cuelist.contents:
     print(type(o))
     print(o)
     if isinstance(o, DmxCue):
         print('Dmx scene, universe0, channel0, value : {}'.format(o.scene.universe(0).channel(0)))
-for o in store.floating_cuelist:
-    print(type(o))
-    print(o)
-    if isinstance(o, DmxCue):
-        print('Dmx scene universe0, channel0 value:{}'.format(o.scene.universe(0).channel(0)))
 
-        
 
 # %%
