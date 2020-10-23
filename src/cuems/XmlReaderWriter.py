3f32945088a193c5870f2a81c2130c6c13647d52
Ion Reguera <ion@stagelab.net>
2020-10-23 14:54:44 +0200

    Add correct namespaces to XML tree, fix validation

--- src/cuems/XmlReaderWriter.py
+++ src/cuems/XmlReaderWriter.py
@@ -6,6 +6,7 @@ import xmlschema
 import datetime  as DT
 import os
 import json
+from pprint import pprint
 
 from .log import logger
 from .CTimecode import CTimecode
@@ -15,7 +16,7 @@ from .XmlBuilder import XmlBuilder
 
 
 class CuemsXml():
-    def __init__(self,schema=None,xmlfile=None):
+    def __init__(self, schema=None, xmlfile=None, namespace={'cms':'http://stagelab.net/cuems'}):
         self.converter = CMLCuemsConverter
         self.schema_object = None
         self._xmlfile = None
@@ -23,6 +24,7 @@ class CuemsXml():
         self.xmlfile = xmlfile
         self.xmldata = None
         self._schema = None
+        self.namespace = namespace
         
     
     @property
@@ -58,16 +60,16 @@ class XmlWriter(CuemsXml):
 
 
     def write(self, xml_data, ):
-    #    self.schema_object.validate(xml_data)
-        ET.ElementTree(xml_data).write(self.xmlfile, encoding="unicode", xml_declaration=True)
+        self.schema_object.validate(xml_data)
+        xml_data.write(self.xmlfile, encoding="utf-8", xml_declaration=True)
 
     def write_from_dict(self, project_dict):
         project_object = CuemsParser(project_dict).parse()
-        xml_data = XmlBuilder(project_object).build()
+        xml_data = XmlBuilder(project_object, namespace=self.namespace, xsd_path=self.schema).build()
         self.write(xml_data)
 
     def write_from_object(self, project_object):
-        xml_data = XmlBuilder(project_object).build()
+        xml_data = XmlBuilder(project_object, namespace=self.namespace, xsd_path=self.schema).build()
         self.write(xml_data)
 
 
@@ -76,11 +78,15 @@ class XmlReader(CuemsXml):
       super().__init__(schema, xmlfile)
 
     def read(self):
-        xml_dict = self.schema_object.to_dict(self.xmlfile, validation='strict',  strip_namespaces=True)
-        # remove namespace infos from xml 
-        del xml_dict['xmlns:cms']
-        del xml_dict['xmlns:xsi']
-        del xml_dict['xsi:schemaLocation']
+        xml_dict = self.schema_object.to_dict(self.xmlfile, validation='strict',  strip_namespaces=False)
+        # remove namespace info from xml 
+        try:
+            del xml_dict['xmlns:cms']
+            del xml_dict['xmlns:xsi']
+            del xml_dict['xsi:schemaLocation']
+        except KeyError:
+            logger.warning('Error triying to remove namespace info on read')
+
         return xml_dict
 
     def read_to_objects(self):
