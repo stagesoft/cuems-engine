f957176a494a2c0bd14cd94cdab531f3f3cbaa73
Ion Reguera <ion@stagelab.net>
2020-10-23 18:36:38 +0200

    fixx XmlWriter init modify test files
    remove prints and pprint import

--- src/cuems/XmlReaderWriter.py
+++ src/cuems/XmlReaderWriter.py
@@ -6,7 +6,6 @@ import xmlschema
 import datetime  as DT
 import os
 import json
-from pprint import pprint
 
 from .log import logger
 from .CTimecode import CTimecode
@@ -16,14 +15,14 @@ from .XmlBuilder import XmlBuilder
 
 
 class CuemsXml():
-    def __init__(self, schema=None, xmlfile=None, namespace={'cms':'http://stagelab.net/cuems'}):
+    def __init__(self, schema, xmlfile=None, namespace={'cms':'http://stagelab.net/cuems'}):
         self.converter = CMLCuemsConverter
         self.schema_object = None
         self._xmlfile = None
+        self._schema = None
         self.schema = schema
         self.xmlfile = xmlfile
         self.xmldata = None
-        self._schema = None
         self.namespace = namespace
         
     
@@ -55,10 +54,6 @@ class CuemsXml():
     
 class XmlWriter(CuemsXml):
 
-    def __init__(self,schema=None,xmlfile=None):
-      super().__init__(schema, xmlfile)
-
-
     def write(self, xml_data, ):
         self.schema_object.validate(xml_data)
         xml_data.write(self.xmlfile, encoding="utf-8", xml_declaration=True)
@@ -69,13 +64,13 @@ class XmlWriter(CuemsXml):
         self.write(xml_data)
 
     def write_from_object(self, project_object):
+        print(self.schema)
         xml_data = XmlBuilder(project_object, namespace=self.namespace, xsd_path=self.schema).build()
         self.write(xml_data)
 
 
 class XmlReader(CuemsXml):
-    def __init__(self,schema=None,xmlfile=None):
-      super().__init__(schema, xmlfile)
+ 
 
     def read(self):
         xml_dict = self.schema_object.to_dict(self.xmlfile, validation='strict',  strip_namespaces=False)
