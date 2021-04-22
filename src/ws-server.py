2d3af59876f8f80bcee54e681d6940f82aca7681
Ion Reguera <ion@stagelab.net>
2021-04-22 10:47:32 +0200

    update editor and deploy sub modules update ws-server test launcher

--- src/ws-server.py
+++ src/ws-server.py
@@ -17,6 +17,8 @@ settings_dict['tmp_upload_path'] = '/tmp/cuemsuploads'
 settings_dict['database_name'] = 'project-manager.db'
 
 
+mappings_dict = {'number_of_nodes': 1, 'default_audio_input': '0367f391-ebf4-48b2-9f26-000000000001_system:capture_1', 'default_audio_output': '0367f391-ebf4-48b2-9f26-000000000001_system:playback_1', 'default_video_input': None, 'default_video_output': '0367f391-ebf4-48b2-9f26-000000000001_0', 'default_dmx_input': None, 'default_dmx_output': None, 'nodes': [{'uuid': '0367f391-ebf4-48b2-9f26-000000000001', 'mac': '2cf05d21cca3', 'audio': {'outputs': [{'name': 'system:playback_1', 'mappings': [{'mapped_to': 'system:playback_1'}]}, {'name': 'system:playback_2', 'mappings': [{'mapped_to': 'system:playback_2'}]}], 'inputs': [{'name': 'system:capture_1', 'mappings': [{'mapped_to': 'system:capture_1'}]}, {'name': 'system:capture_2', 'mappings': [{'mapped_to': 'system:capture_2'}]}]}, 'video': {'outputs': [{'name': '0', 'mappings': [{'mapped_to': '0'}]}]}, 'dmx': None}]}
+
 try:
     if not os.path.exists(settings_dict['tmp_upload_path']):
         os.mkdir(settings_dict['tmp_upload_path'])
@@ -28,7 +30,7 @@ def f(text):
     editor_queue.put(text)
 
 
-server = CuemsWsServer(engine_queue, editor_queue, settings_dict)
+server = CuemsWsServer(engine_queue, editor_queue, settings_dict, mappings_dict)
 logger.info('start server')
 server.start(9092)
 
