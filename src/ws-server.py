e9fbce41f39cac30ad671cc3b10fc000c698995a
Ion Reguera <ion@stagelab.coop>
2025-02-28 18:11:33 +0100

    remove python queue from starter

--- src/ws-server.py
+++ src/ws-server.py
@@ -7,8 +7,7 @@ import time
 import uuid
 import os
 
-engine_queue = Queue()
-editor_queue = Queue()
+
 
 settings_dict = {}
 settings_dict['session_uuid'] = str(uuid.uuid1())
@@ -26,50 +25,13 @@ try:
 except Exception as e:
     print("error: {} {}".format(type(e), e))
 
-def f(text):
-    editor_queue.put(text)
 
 
-server = CuemsWsServer(engine_queue, editor_queue, settings_dict, mappings_dict)
+
+server = CuemsWsServer(settings_dict, mappings_dict)
 logger.info('start server')
 time.sleep(5)
 server.start(9092)
 
-f('playing')
-
-time.sleep(5)
-f('cue 2 50%')
-time.sleep(1)
-f('cue 2 55%')
-
-time.sleep(1)
-f('cue 2 60%')
-f('cue 3 5%')
-f('cue 4 60%')
-time.sleep(1)
-f('cue 5 5%')
-time.sleep(2)
-f('cue 6 60%')
-time.sleep(2)
-f('cue 7 5%')
-time.sleep(1)
-f('cue 8 60%')
-time.sleep(1)
-f('cue 9 5%')
-time.sleep(2)
-f('cue 10 60%')
-time.sleep(2)
-f('cue 11 5%')
-time.sleep(2)
-f('cue 12 60%')
-time.sleep(2)
-f('cue 13 5%')
-time.sleep(2)
-f('cue 14 60%')
-f('cue 15 5%')
-
-
-time.sleep(20)
-f('cue 2 80%')
 
 #server.stop()
